896e98a9c07d81452a9f77aff641591c8622a2d7
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-01-29 12:13:07 +0900

    sceneの概念・並列実行などできた。

--- js/Plot.js
+++ js/Plot.js
@@ -1,24 +1,74 @@
-function Plot (opts) {
-    this.scenes = [];
+function Plot (sequence) {
+    this.sequence = [];
+    this.scenes = {};
+
+    if (sequence) {
+        this.setSequence(sequence);
+    }
 }
 
-Plot.prototype.play = function (immediate) {
-    if (immediate) {
-        this.setScenes(immediate);
+Plot.prototype.play = function (sequence) {
+    if (sequence) {
+        this.setSequence(sequence);
     }
 
-    var scenes = this.scenes || [];
+    sequence = this.sequence || [];
 
+    var self = this;
     (function exec (index) {
-        if (!scenes[index]) {
+        if (!sequence[index]) {
             return;
         }
-        scenes[index](function () {
+
+        function done () {
             exec(index + 1);
-        });
+        }
+
+        var scene = sequence[index];
+        var sceneName;
+        if (typeof scene != 'string') {
+            sceneName = ['scene', +(new Date())].join('_');
+            self.cut(sceneName, scene);
+        } else {
+            sceneName = scene;
+        }
+
+        self.playScene(sceneName, done);
     })(0);
 };
 
-Plot.prototype.setScenes = function (scenes) {
-    this.scenes = scenes;
+Plot.prototype.setSequence = function (sequence) {
+    this.sequence = sequence;
+};
+
+Plot.prototype.playScene = function (sceneName, callback) {
+    var self = this;
+    var scene = this.scenes[sceneName];
+
+    var cutCount = scene.length;
+    var doneCount = 0;
+
+    function done () {
+        doneCount++;
+        if (cutCount == doneCount) {
+            setTimeout(callback);
+        }
+    }
+
+    for (var i = 0; i < cutCount; i++) {
+        (function (cut) {
+            setTimeout(function () {
+                cut.apply(self, [done]);
+            });
+        })(scene[i]);
+    }
+};
+
+Plot.prototype.cut = function (sceneName, cut) {
+    var scene = this.scenes[sceneName];
+    if (!scene) {
+        scene = [];
+    }
+    scene.push(cut);
+    this.scenes[sceneName] = scene;
 };
--- src/test/plot-test.js
+++ src/test/plot-test.js
@@ -1,7 +1,7 @@
 var assert = chai.assert;
 
 describe('plot', function () {
-    it('exec functions in order', function (done) {
+    it('play functions in order', function (done) {
         var plot = new Plot();
         var array = [];
 
@@ -21,5 +21,68 @@ describe('plot', function () {
             assert.equal(array.join(''), '1234');
             done();
         }]);
+    });
+
+    it('write scenes and asign', function (done) {
+        var plot = new Plot([
+            'set_a',
+            'set_b',
+            'set_c',
+            'end'
+        ]);
+
+        var hoge = null;
+
+        plot.cut('set_a', function (next) {
+            hoge = 'a';
+            next();
+        });
+
+        plot.cut('set_b', function (next) {
+            assert.equal(hoge, 'a');
+            hoge = 'b';
+            next();
+        });
+
+        plot.cut('set_c', function (next) {
+            assert.equal(hoge, 'b');
+            hoge = 'c';
+            next();
+        });
+
+        plot.cut('end', function (next) {
+            assert.equal(hoge, 'c');
+            done();
+        });
+
+        plot.play();
+    });
+
+    it('parallel', function (done) {
+        var plot = new Plot([
+            'set_value',
+            'end'
+        ]);
+
+        var hoge = null;
+        var moge = null;
+
+        plot.cut('set_value', function (next) {
+            hoge = 'a';
+            next();
+        });
+
+        plot.cut('set_value', function (next) {
+            moge = 'b';
+            next();
+        });
+
+        plot.cut('end', function (next) {
+            assert.equal(hoge, 'a');
+            assert.equal(moge, 'b');
+            done();
+        });
+
+        plot.play();
     });
 });
